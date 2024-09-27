5a8d88eba9bec72a2518ad9d79f8e44c6c7e429f
rose.dimelow <rose.dimelow@MU-C1-22.mullen.pvt.k12.co.us>
2024-09-27 11:38:04 -0600

    initial commit

---
+++ LeapYearChallenge/src/LeapYearTask.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class LeapYearTask 
+
+{
+
+	public static void main(String[] args) 
+	
+	{
+		Scanner userInput = new Scanner (System.in);
+		System.out.println("Type in a year!");
+		
+		int year = userInput.nextInt();
+		
+		if (year % 4 == 0 & year % 100 != 0 | year % 4== 0 & year % 400 == 0)
+		{
+			System.out.println("This year is a leap year!  :)");
+		}
+		
+		else
+		{
+			System.out.println("This year is not a leap year.  :(");
+		}
+			
+	}
+
+}
